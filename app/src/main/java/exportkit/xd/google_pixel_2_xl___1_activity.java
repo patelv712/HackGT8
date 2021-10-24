1d01504ba8b6ae7da55ef24cedd366ca15f1fc34
Varun P <37121364+patelv712@users.noreply.github.com>
2021-10-23 22:49:45 -0400

    Create splash screen and go to home screen

--- app/src/main/java/exportkit/xd/google_pixel_2_xl___1_activity.java
+++ app/src/main/java/exportkit/xd/google_pixel_2_xl___1_activity.java
@@ -1,19 +1,17 @@
+/*
+ *	This content is generated from the API File Info.
+ *	(Alt+Shift+Ctrl+I).
+ *
+ *	@desc
+ *	@file 		hackgt_8___wtm
+ *	@date 		1635025335591
+ *	@title 		Page 1
+ *	@author
+ *	@keywords
+ *	@generator 	Export Kit v1.3.figma
+ *
+ */
 
-	 
-	/*
-	 *	This content is generated from the API File Info.
-	 *	(Alt+Shift+Ctrl+I).
-	 *
-	 *	@desc 		
-	 *	@file 		hackgt_8___wtm
-	 *	@date 		1635025335591
-	 *	@title 		Page 1
-	 *	@author 	
-	 *	@keywords 	
-	 *	@generator 	Export Kit v1.3.figma
-	 *
-	 */
-	
 
 package exportkit.xd;
 
@@ -27,40 +25,39 @@ import android.widget.TextView;
 
 public class google_pixel_2_xl___1_activity extends Activity {
 
-	
-	private View _bg__google_pixel_2_xl___1_ek2;
-	private ImageView vector_1;
-	private TextView wtm;
-	private TextView what_s_the_move_;
-	private View rectangle_2;
-	private ImageView vector;
-	private ImageView vector_ek1;
-	private View rectangle_3;
-	private ImageView vector_ek2;
-	private ImageView vector_ek3;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+    private View _bg__google_pixel_2_xl___1_ek2;
+    private ImageView vector_1;
+    private TextView wtm;
+    private TextView what_s_the_move_;
+    private View rectangle_2;
+    private ImageView vector;
+    private ImageView vector_ek1;
+    private View rectangle_3;
+    private ImageView vector_ek2;
+    private ImageView vector_ek3;
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.google_pixel_2_xl___1);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
 
-		
-		_bg__google_pixel_2_xl___1_ek2 = (View) findViewById(R.id._bg__google_pixel_2_xl___1_ek2);
-		vector_1 = (ImageView) findViewById(R.id.vector_1);
-		wtm = (TextView) findViewById(R.id.wtm);
-		what_s_the_move_ = (TextView) findViewById(R.id.what_s_the_move_);
-		rectangle_2 = (View) findViewById(R.id.rectangle_2);
-		vector = (ImageView) findViewById(R.id.vector);
-		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
-		rectangle_3 = (View) findViewById(R.id.rectangle_3);
-		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
-		vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
-	
-		
-		//custom code goes here
-	
-	}
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.google_pixel_2_xl___1);
+
+
+        _bg__google_pixel_2_xl___1_ek2 = (View) findViewById(R.id._bg__google_pixel_2_xl___1_ek2);
+        vector_1 = (ImageView) findViewById(R.id.vector_1);
+        wtm = (TextView) findViewById(R.id.wtm);
+        what_s_the_move_ = (TextView) findViewById(R.id.what_s_the_move_);
+        rectangle_2 = (View) findViewById(R.id.rectangle_2);
+        vector = (ImageView) findViewById(R.id.vector);
+        vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+        rectangle_3 = (View) findViewById(R.id.rectangle_3);
+        vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+        vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
+
+
+        //custom code goes here
+
+    }
 }
-	
 	
--- app/src/main/java/exportkit/xd/hackgt_8___wtm_activity.java
+++ app/src/main/java/exportkit/xd/hackgt_8___wtm_activity.java
@@ -20,8 +20,16 @@ package exportkit.xd;
 import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class hackgt_8___wtm_activity extends Activity {
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import android.content.Intent;
+
+
+public class hackgt_8___wtm_activity extends AppCompatActivity {
 
 	
 
@@ -32,12 +40,20 @@ public class hackgt_8___wtm_activity extends Activity {
 		//setContentView(R.layout.hackgt_8___wtm);
 		setContentView(R.layout.google_pixel_2_xl___1);
 
-		
-	
-		
+		getSupportActionBar().hide();
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Intent intent = new Intent(hackgt_8___wtm_activity.this, google_pixel_2_xl___2_activity.class);
+				startActivity(intent);
+				finish(); //if back button finish the program
+			}
+		}, 4000);
+	}
 		//custom code goes here
 	
 	}
-}
 	
 	
